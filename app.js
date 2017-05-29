5a6057aece3ad16d7ccac285f4fdf5c8c3d8763e
Helder Ferreira <woounn@gmail.com>
2017-05-29 13:54:54 +0100

    Upload done

--- app.js
+++ app.js
@@ -1,60 +1,81 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var Clarifai = require('clarifai');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+var vision = require('@google-cloud/vision')({
+  keyFilename: './keys/My Project-5b474438de4d.json'
 });
 
-app.get('/',function (req, res) {
-    res.send('Hello World!')
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-var appClarifai = new Clarifai.App(
-  'XH9-BKSIzEO7Ympw8z7IpzvnILYUzgMoQl0ymbTq',
-  'O6VgBPZA07w4dkhQKtgm8B7IoJiNypFfCmsrjZ8Z'
-);
+app.post('/upload', function(req, res){
 
-appClarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-    function(response) {
-        var arrayPreditions = [];
-        
-        var jsonReceived = response.outputs[0].data.concepts;
-        var cenas =  jsonReceived.length;
-        // console.log(cenas);
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
 
-        for(i=0;i<jsonReceived.length;i++) {
-            arrayPreditions.push(jsonReceived[i].name);
-        }
-        console.log(arrayPreditions);
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+    
+    form.filePath = form.uploadDir+'\\'+file.name;
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function(field,file) {
+    var finalArray = [];
+    var numElements = 0;
+
+    vision.detectLabels(form.filePath)
+    .then((results) => {
         
+        var usedArray = results[1].responses[0].labelAnnotations;
+        numElements=usedArray.length;
         
-        
-    },
-    function(err) {
-        console.error(err);
+        usedArray.forEach(function(element) {
+          var auxObj = {};
+          auxObj.name = element.description;
+          auxObj.percentage = element.score;
+          finalArray.push(auxObj);
+          deCount();
+        }, this);
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+    });
+
+    // Assync method
+
+    function deCount() {
+       numElements--;
+       if (numElements<1) {
+         res.json(finalArray);
+       }
     }
-);
+    
+  });
 
+  // parse the incoming request containing the form data
+  form.parse(req);
 
-app.listen(3000);
-console.log("Listening on port 3000");
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
